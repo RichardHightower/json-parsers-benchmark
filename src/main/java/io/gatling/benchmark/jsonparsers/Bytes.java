adcd0614c983c9949bc1588980f4286d7e2a2779
javaboon <richard.m.hightower@gmail.com>
2013-12-10 21:34:29 -0800

    added a few more types to the JSON test

--- src/main/java/io/gatling/benchmark/jsonparsers/Bytes.java
+++ src/main/java/io/gatling/benchmark/jsonparsers/Bytes.java
@@ -16,8 +16,22 @@ public class Bytes {
     public static final byte[] WEBXML_BYTES = readBytes( "data/webxml.json" );
     public static final byte[] WIDGET_BYTES = readBytes( "data/widget.json" );
 
+    public static final String STR_ACTION_LABEL_BYTES = readStr( "data/actionLabel.json" );
+    public static final String STR_CITM_CATALOG_BYTES = readStr( "data/citm_catalog.json" );
+    public static final String STR_MEDIUM_BYTES = readStr( "data/medium.json" );
+    public static final String STR_MENU_BYTES = readStr( "data/menu.json" );
+    public static final String STR_SGML_BYTES = readStr( "data/sgml.json" );
+    public static final String  STR_SMALL_BYTES = readStr( "data/small.json" );
+    public static final String STR_WEBXML_BYTES = readStr( "data/webxml.json" );
+    public static final String STR_WIDGET_BYTES = readStr( "data/widget.json" );
+
     private static byte[] readBytes(String path) {
             return IO.read (path ).getBytes ( StandardCharsets.UTF_8 );
 
+    }
+
+
+    private static String readStr(String path) {
+        return IO.read (path );
     }
 }
