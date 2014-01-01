4727b8965d718566864a200d07e492fb88111c3e
Richard Hightower <richardhightower@gmail.com>
2013-12-31 19:12:45 -0800

    A few more rounds of optimizing the byte array direct parsing.

--- src/main/java/io/gatling/jsonbenchmark/bytes/BoonBenchMarkUTF8Bytes.java
+++ src/main/java/io/gatling/jsonbenchmark/bytes/BoonBenchMarkUTF8Bytes.java
@@ -1,10 +1,9 @@
-package io.gatling.jsonbenchmark.string;
+package io.gatling.jsonbenchmark.bytes;
 
 import org.boon.json.JsonParser;
-import org.boon.json.implementation.JsonParserCharArray;
-import org.boon.json.implementation.JsonParserConcurrent;
+import org.boon.json.JsonParserFactory;
+import org.boon.json.implementation.JsonUTF8Parser;
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
-import org.openjdk.jmh.annotations.OperationsPerInvocation;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.logic.BlackHole;
@@ -13,66 +12,64 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import static io.gatling.jsonbenchmark.bytes.Buffers.*;
-import static io.gatling.jsonbenchmark.bytes.Buffers.STR_WIDGET_BYTES;
+import static io.gatling.jsonbenchmark.bytes.Buffers.WIDGET_BYTES;
 
-
+/**
+ * Created by rick on 12/31/13.
+ */
 @State
-public class BoonClassicBenchmark {
+public class BoonBenchMarkUTF8Bytes {
+    private final JsonParser parser = new JsonUTF8Parser();
 
-    private final JsonParser parser = new JsonParserCharArray (  );
-
-    private Object parse(String str) throws Exception {
-        return parser.parse ( Map.class, str );
+    private Object parse(byte[] bytes) throws Exception {
+        return parser.parse ( Map.class,  bytes );
     }
-
-
 
     @GenerateMicroBenchmark
     @OutputTimeUnit ( TimeUnit.SECONDS)
     public void actionLabel(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_ACTION_LABEL_BYTES));
+        bh.consume(parse(ACTION_LABEL_BYTES));
     }
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void citmCatalog(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_CITM_CATALOG_BYTES));
+        bh.consume(parse(CITM_CATALOG_BYTES));
     }
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void medium(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_MEDIUM_BYTES));
+        bh.consume(parse(MEDIUM_BYTES));
     }
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void menu(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_MENU_BYTES));
+        bh.consume(parse(MENU_BYTES));
     }
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void sgml(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_SGML_BYTES));
+        bh.consume(parse(SGML_BYTES));
     }
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void small(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_SMALL_BYTES));
+        bh.consume(parse(SMALL_BYTES));
     }
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void webxml(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_WEBXML_BYTES));
+        bh.consume(parse(WEBXML_BYTES));
     }
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
     public void widget(BlackHole bh) throws Exception {
-        bh.consume(parse(STR_WIDGET_BYTES));
+        bh.consume(parse(WIDGET_BYTES));
     }
 }
-
