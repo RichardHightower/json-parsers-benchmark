758a378584cd65df97345cc24a3e9cd721b25082
javaboon <richard.m.hightower@gmail.com>
2014-03-16 14:24:13 -0700

    benchmarks

--- src/main/java/org/boon/ReduceBy.java
+++ src/main/java/org/boon/ReduceBy.java
@@ -1,5 +1,7 @@
 package org.boon;
 
+import org.boon.collections.IntList;
+import org.boon.core.Fn;
 import org.boon.primitive.Int;
 import org.openjdk.jmh.annotations.GenerateMicroBenchmark;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
@@ -17,10 +19,10 @@ public class ReduceBy {
     final static Object func = new Functions();
 
     static {
-        int [] a = new int[50_000];
+        int [] a = new int[5_000_000];
 
         for (int index = 0; index < a.length; index++) {
-            a[index] = index;
+            a[index] = 1;
         }
 
         array = a;
@@ -33,6 +35,7 @@ public class ReduceBy {
     }
 
     public long sumReduceByUsingInterface() {
+
         return Int.reduceBy(array, new Int.ReduceBy(){
 
             @Override
@@ -42,9 +45,27 @@ public class ReduceBy {
         });
     }
 
+    public long sumIntListReduceByUsingInterface() {
+        IntList list = new IntList(array);
 
-    public long sumReduceByReflection() {
-        return Int.reduceBy(array, 0, array.length, func);
+        return list.reduceBy(new Int.ReduceBy(){
+
+            @Override
+            public long reduce(long sum, int value) {
+                return sum+value;
+            }
+        });
+    }
+
+
+    public long sumReduceByDirectReflection() {
+
+        //Using
+        return Int.reduceBy(array, new Fn() {
+            long f(long sum, int value) {
+                return sum + value;
+            }
+        });
     }
 
 
@@ -64,15 +85,17 @@ public class ReduceBy {
 
     @GenerateMicroBenchmark
     @OutputTimeUnit(TimeUnit.SECONDS)
+    public void sumIntListReduceByUsingInterfaceBench(BlackHole bh) throws Exception {
+        bh.consume(sumIntListReduceByUsingInterface());
+    }
+
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
     public void sumReduceByUsingInvokeDynamicBench(BlackHole bh) throws Exception {
         bh.consume(sumReduceByUsingInvokeDynamic());
     }
 
-    @GenerateMicroBenchmark
-    @OutputTimeUnit(TimeUnit.SECONDS)
-    public void sumReduceByReflectionBench(BlackHole bh) throws Exception {
-        bh.consume(sumReduceByReflection());
-    }
 
 
     @GenerateMicroBenchmark
@@ -93,6 +116,13 @@ public class ReduceBy {
     public void sumReduceByUsingInterfaceBench(BlackHole bh) throws Exception {
         bh.consume(sumReduceByUsingInterface());
     }
+
+    @GenerateMicroBenchmark
+    @OutputTimeUnit(TimeUnit.SECONDS)
+    public void sumReduceByDirectReflectionBench(BlackHole bh) throws Exception {
+        bh.consume(sumReduceByDirectReflection());
+    }
+
 
 
 
